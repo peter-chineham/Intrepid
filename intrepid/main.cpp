70fc4a885b1f3a5ca3704aabfde6070f2770eb07
peter-chineham <peter@vinntec.co.uk>
2018-06-05 14:31:00 +0100

    Convert to VS

--- intrepid/main.cpp
+++ intrepid/main.cpp
@@ -4,8 +4,8 @@
 int main(int argc, char *argv[])
 {
     string argv_str(argv[0]); // Linux + Windows
-    MainWindow::base = argv_str.substr(0, argv_str.find("build"));
-    MainWindow::base += "intrepid\\Resources\\";
+    MainWindow::base = argv_str.substr(0, argv_str.find("\\intrepid\\intrepid\\") + 19);
+    MainWindow::base += "Resources\\";
 
     QApplication a(argc, argv);
     MainWindow w;
